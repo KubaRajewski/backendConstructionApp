5294355b1ab78730456dff56149939a0c6c2ffff
Kuba Rajewski <kuba.rajewski0@gmail.com>
2025-05-07 16:24:08 +0200

    Innitial commit

---
+++ src/main/java/com/example/backendcontstructionapp/BackendContstructionAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.backendcontstructionapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendContstructionAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendContstructionAppApplication.class, args);
+    }
+
+}
